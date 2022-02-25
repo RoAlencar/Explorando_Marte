d08804b8806c3aafd1eabb6e59806567555c0865
RoAlencar <roalencarx@gmail.com>
2022-02-25 12:43:09 -0300

    ExplorandoMarte fix: Removing unnecessary classes

--- ExplorandoMarte/src/main/java/br/com/raxinformatica/explorandomarte/exceptions/PlanetException.java
+++
@@ -1,9 +0,0 @@
-package br.com.raxinformatica.explorandomarte.exceptions;
-
-public class PlanetException extends Exception{
-    private static final long serialVersionUID = -4970527916966267734L;
-
-    public PlanetException(String message) {
-        super (message);
-    }
-}
--- ExplorandoMarte/src/main/java/br/com/raxinformatica/explorandomarte/exceptions/SpaceshipException.java
+++
@@ -1,9 +0,0 @@
-package br.com.raxinformatica.explorandomarte.exceptions;
-
-public class SpaceshipException extends Exception{
-    private static final long serialVersionUID = -4970527916966267734L;
-
-    public SpaceshipException(String message){
-        super (message);
-    }
-}
